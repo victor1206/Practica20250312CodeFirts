beb15bfd4931f60ccf6686ab04e3297d90b644e1
victor1206 <victor_rivas89@hotmail.com>
2025-03-18 19:57:37 -0600

    mi primer commit

---
+++ Practica20250312G06.MVCCodeFirst/Models/ESFEDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica20250312G06.MVCCodeFirst.Models
+{
+    public class ESFEDbContext : DbContext
+    {
+        public ESFEDbContext(DbContextOptions<ESFEDbContext> options) : base(options) { }
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+    }
+}
---
+++ Practica20250312G06.MVCCodeFirst/Models/Proveedor.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica20250312G06.MVCCodeFirst.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+    }
+}
