5f955a4af89924a8c6b7beb8d0fb5c9bfc9cbee6
victor1206 <victor_rivas89@hotmail.com>
2025-03-18 20:01:51 -0600

    Se modifico el inicio de la vista a create

--- Practica20250312G06.MVCCodeFirst/Program.cs
+++ Practica20250312G06.MVCCodeFirst/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Proveedor}/{action=Create}/{id?}");
 
 app.Run();
