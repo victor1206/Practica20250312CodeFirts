8f4e70e1bc015e20f06224e292e44f8fafe5be50
victor1206 <victor_rivas89@hotmail.com>
2025-03-18 20:04:33 -0600

    Se dejo nuevamente el inicio del proyecto con home

--- Practica20250312G06.MVCCodeFirst/Program.cs
+++ Practica20250312G06.MVCCodeFirst/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Proveedor}/{action=Create}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
